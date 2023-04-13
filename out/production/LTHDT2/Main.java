e3bd52a6df9f8c7bf6792e61513f3630e889fc7c
tu4n-ibs <114391251+tu4n-ibs@users.noreply.github.com>
2023-04-13 21:43:51 +0700

    baitap4

---
+++ out/production/LTHDT2/Main.java
@@ -0,0 +1,8 @@
+package baitap4;
+
+public class Main {
+    public static void main(String[] args) {
+        Traingle traingle = new Traingle(2,2,2);
+        System.out.println(traingle);
+    }
+}
