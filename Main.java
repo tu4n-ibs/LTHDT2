075c531d80022556fad0b92a5444669e03dd6820
tu4n-ibs <114391251+tu4n-ibs@users.noreply.github.com>
2023-04-13 21:42:37 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package baitap4;
-
-public class Main {
-    public static void main(String[] args) {
-        Traingle traingle = new Traingle(2,2,2);
-        System.out.println(traingle);
-    }
-}
