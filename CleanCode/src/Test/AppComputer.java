ae0105aecdf3f6f8570999141ebd4ea7465236ef
tu4n-ibs <114391251+tu4n-ibs@users.noreply.github.com>
2023-05-29 12:05:45 +0700

    Delete CleanCode directory

--- CleanCode/src/Test/AppComputer.java
+++
@@ -1,9 +0,0 @@
-package Test;
-
-public class AppComputer {
-    public static void main(String[] args) {
-//        Computer[]computer0 = new Computer[0];
-//        Computer computer = new Computer("ms000","macbook","Apple","1111",2143254,true);
-//        computer.addComputerToArray(computer,computer0);
-    }
-}
--- CleanCode/src/Test/Main.java
+++
@@ -1,8 +0,0 @@
-package Test;
-
-public class Main {
-    public static void main(String[] args) {
-        People people = new People("ivan",18,true,1.75,59.0);
-        System.out.println( people.toString());
-    }
-}
