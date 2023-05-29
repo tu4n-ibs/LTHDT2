8be0d3e28b7350688dcdeea0d737b8942e810966
tu4n-ibs <114391251+tu4n-ibs@users.noreply.github.com>
2023-05-29 13:06:10 +0700

    kiemtra

---
+++ CleanCode/src/Test/Teacher.java
@@ -0,0 +1,25 @@
+package Test;
+
+public class Teacher extends People {
+    String level;
+    String experience;
+    String subject;
+
+    public Teacher() {
+        super();
+    }
+
+    public Teacher(String name, int age, String sex, double height, double weight, String level, String experience, String subject) {
+        super(name, age, sex , height, weight);
+        this.level = level;
+        this.experience = experience;
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "level: "+ level
+                +"experience: "+ experience
+                +"subject: "+ subject;
+    }
+}
